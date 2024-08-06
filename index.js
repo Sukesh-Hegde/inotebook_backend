dbf2dd79bbff1602ea33d1e2767df63fd8a7fe02
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-06 19:15:13 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-import express from 'express'
-import { connectUsingMongoose } from './db.js';
-import authRouter from './routes/auth.js';
-import noteRouter from './routes/notes.js';
-import cors from 'cors'
+import express from "express";
+import { connectUsingMongoose } from "./db.js";
+import authRouter from "./routes/auth.js";
+import noteRouter from "./routes/notes.js";
+import cors from "cors";
 
 connectUsingMongoose();
 const app = express();
 app.use(cors());
+app.use(cors({ origin: "https://inotebook-8btc.onrender.com" }));
+
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.use("https://inotebook-backend-rrv6.onrender.com/api/auth", authRouter);
-app.use("https://inotebook-backend-rrv6.onrender.com/api/notes", noteRouter);
-
+app.use("/api/auth", authRouter);
+app.use("/api/notes", noteRouter);
 
 app.listen(port, () => {
   console.log(`iNotebook app listening at http://localhost:${port}`);
 });
-
-
-
-
